85dda649a60e7d7c048f84a5c812d793ef581774
Unknown <112800849+JustGithubProject@users.noreply.github.com>
2023-07-18 16:30:36 +0200

    Update views.py

--- bboard/views.py
+++ bboard/views.py
@@ -158,7 +158,7 @@ def user_sell(request):
 @login_required
 def create_product(request):
     if request.method == 'POST':
-        form = ProductForm(request.POST)
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save(commit=False)
             product.user = request.user
