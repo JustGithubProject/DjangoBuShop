bb5670ab9892c1d64c67e4c13bdb410847fc5c0b
JustGithubProject <Kropivadanil@gmail.com>
2023-10-07 08:42:27 +0200

    тесты к bboard views

--- bboard/tests/test_views.py
+++ bboard/tests/test_views.py
@@ -1,7 +1,10 @@
 from django.urls import reverse
 
+from accounts.models import User
 from bboard.forms import ReviewForm
 from django.test import TestCase
+
+from bboard.models import Product
 
 
 class HomeViewTest(TestCase):
@@ -10,3 +13,20 @@ class HomeViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/new/index.html')
         self.assertIsInstance(response.context['form'], ReviewForm)
+
+
+class SearchViewTest(TestCase):
+    def test_search_with_query(self):
+        response = self.client.get(reverse("search"), {"query": "Тестовый"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["query"], "Тестовый")
+        self.assertTemplateUsed(response, 'products/search_results.html')
+
+
+class GetProductViewTest(TestCase):
+    def test_get_products(self):
+        response = self.client.get(reverse("get_products"), {"category": "1", "page": "1"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/new/product.html")
+
+
