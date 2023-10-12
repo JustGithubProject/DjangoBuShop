f4e5409e1c54745d7cb46d583a381ce9533dcb5e
JustGithubProject <Kropivadanil@gmail.com>
2023-10-12 15:06:50 +0200

    set up pytest for test

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,77 +1,35 @@
-from django.test import TestCase
+import pytest
 from django.urls import reverse
-
-from ..forms import RegistrationForm
-from ..forms import LoginForm
-
-
-class RegistrationViewTest(TestCase):
-    def test_registration_view_get(self):
-        response = self.client.get(reverse('register'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response.context["form"], RegistrationForm)
-        self.assertTemplateUsed(response, "users/registration.html")
-
-    def test_registration_view_post_valid_data(self):
-        data = {
-            'username': 'testuser',
-            'password1': 'testpassword',
-            'password2': 'testpassword',
-        }
-        response = self.client.post(reverse('register'), data)
-        self.assertEqual(response.status_code, 200)
+from accounts.forms import RegistrationForm
+from ..models import User
+from django.test import Client
 
 
-class LoginViewTest(TestCase):
-    def setUp(self):
-        self.username = "testuser"
-        self.password = "testpassword"
+@pytest.mark.django_db
+def test_registration_view_post_success():
+    client = Client()
+    user_data = {
+        "username": "test_user",
+        "password1": "mCHhKe7rYKnuc",
+        "password2": "mCHhKe7rYKnuc",
+        "email": "test_user@example.com",
+        "phone_number": "+1234567890",
+        "city": "Test City",
+    }
+    response = client.post(reverse("register"), user_data)
+    print(response.content)
+    assert response.status_code == 200
+    assert User.objects.filter(username="test_user").exists()
 
-    def test_login_get(self):
-        response = self.client.get(reverse("login"))
-        self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response.context["form"], LoginForm)
-        self.assertTemplateUsed(response, "users/login.html")
-
-    def test_login_view_post_valid_credentials(self):
-        data = {
-            "username": self.username,
-            "password": self.password
-        }
-        response = self.client.post(reverse("login"), data)
-        self.assertEqual(response.status_code, 200)
-
-
-class LogoutViewTestCase(TestCase):
-    def setUp(self):
-        self.username = "testuser"
-        self.password = "testpassword"
-
-    def test_logout_view(self):
-        response = self.client.get(reverse("logout"))
-        self.assertRedirects(response, reverse("login"))
+    # Clean up the test data
+    User.objects.filter(username="test_user").delete()
 
 
-class ProfileViewTestCase(TestCase):
-    def test_profile_view_get(self):
-        self.client.login(username="username", password="password")
+@pytest.mark.django_db
+def test_registration_view_get():
+    client = Client()
+    response = client.get(reverse("register"))
+    assert response.status_code == 200  # Ожидается успешный ответ
 
-        response = self.client.get(reverse("profile"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "users/profile.html")
-
-
-class PasswordResetViewsTest(TestCase):
-    def test_password_reset_view(self):
-        response = self.client.get(reverse('password_reset'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/password_reset_form.html')
-
-        response = self.client.post(reverse('password_reset'), {'email': "Kropivadanil@gmail.com"})
-        self.assertEqual(response.status_code, 302)  # Ожидаем редирект
-        # Добавьте проверку отправки письма сброса пароля, если это требуется
-
-    def test_password_reset_done_view(self):
-        response = self.client.get(reverse('password_reset_done'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/password_reset_done.html')
+    # Проверка, что форма отображается на странице
+    assert isinstance(response.context["form"], RegistrationForm)
--- bboard/tests/test_views.py
+++ bboard/tests/test_views.py
@@ -7,26 +7,6 @@ from django.test import TestCase
 from bboard.models import Product
 
 
-class HomeViewTest(TestCase):
-    def test_home_view(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'products/new/index.html')
-        self.assertIsInstance(response.context['form'], ReviewForm)
 
-
-class SearchViewTest(TestCase):
-    def test_search_with_query(self):
-        response = self.client.get(reverse("search"), {"query": "Тестовый"})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["query"], "Тестовый")
-        self.assertTemplateUsed(response, 'products/search_results.html')
-
-
-class GetProductViewTest(TestCase):
-    def test_get_products(self):
-        response = self.client.get(reverse("get_products"), {"category": "1", "page": "1"})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/new/product.html")
 
 
