60c920491dda6e89d193f239b47d157a23a708b2
JustGithubProject <Kropivadanil@gmail.com>
2023-10-13 11:14:14 +0200

    wrote first tests using pytest

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -5,31 +5,3 @@ from ..models import User
 from django.test import Client
 
 
-@pytest.mark.django_db
-def test_registration_view_post_success():
-    client = Client()
-    user_data = {
-        "username": "test_user",
-        "password1": "mCHhKe7rYKnuc",
-        "password2": "mCHhKe7rYKnuc",
-        "email": "test_user@example.com",
-        "phone_number": "+1234567890",
-        "city": "Test City",
-    }
-    response = client.post(reverse("register"), user_data)
-    print(response.content)
-    assert response.status_code == 200
-    assert User.objects.filter(username="test_user").exists()
-
-    # Clean up the test data
-    User.objects.filter(username="test_user").delete()
-
-
-@pytest.mark.django_db
-def test_registration_view_get():
-    client = Client()
-    response = client.get(reverse("register"))
-    assert response.status_code == 200  # Ожидается успешный ответ
-
-    # Проверка, что форма отображается на странице
-    assert isinstance(response.context["form"], RegistrationForm)
--- bboard/tests/test_views.py
+++ bboard/tests/test_views.py
@@ -1,10 +1,92 @@
+import pytest
 from django.urls import reverse
-
+from django.test import Client
 from accounts.models import User
 from bboard.forms import ReviewForm
-from django.test import TestCase
-
+from bboard.utils import transliterate
 from bboard.models import Product
+from .. import services
+from ..models import Category
+
+
+@pytest.mark.django_db
+def test_home_view_get_request():
+    client = Client()
+
+    response = client.get(reverse("home"))
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_review_form():
+    post_data = {
+        'reviewer_name': 'TestUser',
+        'content': 'Contentmustbemorethan10symbols',
+    }
+    form = ReviewForm(data=post_data)
+    assert form.is_valid()
+
+
+@pytest.mark.django_db
+def test_get_recent_products_with_users():
+    category1 = Category.objects.create(name='Category 1', slug=transliterate("Category 1"))
+    category2 = Category.objects.create(name='Category 2', slug=transliterate("Category 2"))
+    # Создайте пользователей
+    user1 = User.objects.create(username='user1', password='password1')
+    user2 = User.objects.create(username='user2', password='password2')
+
+    # Создайте продукты, связанные с пользователями
+    product1 = Product.objects.create(
+        user=user1,
+        category=category1,
+        title='Product 1',
+        price=10.0,
+        slug=transliterate('Product 1')
+    )
+    product2 = Product.objects.create(
+        user=user2,
+        category=category1,
+        title='Product 2',
+        price=20.0,
+        slug=transliterate('Product 2')
+    )
+    product3 = Product.objects.create(
+        user=user1,
+        category=category2,
+        title='Product 3',
+        price=15.0,
+        slug=transliterate('Product 3')
+    )
+    product4 = Product.objects.create(
+        user=user1,
+        category=category2,
+        title='Product 4',
+        price=15.0,
+        slug=transliterate('Product 4')
+    )
+    product5 = Product.objects.create(
+        user=user1,
+        category=category2,
+        title='Product 5',
+        price=15.0,
+        slug=transliterate('Product 5')
+    )
+
+    product6 = Product.objects.create(
+        user=user1,
+        category=category2,
+        title='Product 6',
+        price=15.0,
+        slug=transliterate('Product 6')
+    )
+
+    # Вызов функции для проверки
+    recent_products = services.get_recent_products_with_users()
+
+    assert len(recent_products) == 5
+
+
 
 
 
